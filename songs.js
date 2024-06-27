69f5b2768db2d2bea9dc0fc78b6fc0d29e33b51d
Ataídes Alves <chamega.ti@gmail.com>
2024-06-26 21:53:50 -0300

    first commit

---
+++ songs.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        src: "./src/É FOGO.mp3",
+        name: "É FOGO",
+    },
+    {
+        src: "./src/ficar-por-ficar.mp3",
+        name: "ficar-por-ficar",
+    },
+    {
+        src: "./src/locutor.mp3",
+        name: "locutor",
+    },
+    {
+        src: "./src/AMOR INOCENTE.mp3",
+        name: "AMOR INOCENTE",
+    },
+    {
+        src: "./src/boa-de-cama.mp3",
+        name: "boa-de-cama",
+    },
+    {
+        src: "./src/CHUVA DE PAPEL PICADO.mp3",
+        name: "CHUVA DE PAPEL PICADO",
+    },
+    {
+        src: "./src/CORAÇÃO DE PEDRA.mp3",
+        name: "CORAÇÃO DE PEDRA",
+    },
+    {
+        src: "./src/DAÍ ''QUI MIORÔ''.mp3",
+        name: "DAÍ ''QUI MIORÔ''",
+    },
+    {
+        src: "./src/Direto Ao Assunto.mp3",
+        name: "Direto Ao Assunto",
+    },
+    {
+        src: "./src/ESTE AMOR AINDA VAI ME MATAR.mp3",
+        name: "ESTE AMOR AINDA VAI ME MATAR",
+    },
+    {
+        src: "./src/MULHER TRANSGÊNICA.mp3",
+        name: "MULHER TRANSGÊNICA",
+    },
+    {
+        src: "./src/Paixão Bandida.mp3",
+        name: "Paixão Bandida",
+    },
+    {
+        src: "./src/petalas-de-prata.mp3",
+        name: "petalas-de-prata",
+    },
+    {
+        src:"./src/tic-tac-do-relogio.mp3",
+        name: "tic-tac-do-relogio",
+    },
+    {
+        src: "./src/Se Chover Mulher.mp3",
+        name: "Se Chover Mulher",
+    },
+];
